8471ce3fa597f38094c97c3929a03a2865217ef1
recursor94 <andrew.spano1@marist.edu>
2013-09-14 22:13:54 -0400

    completed query 5

--- assignment1.sql
+++ assignment1.sql
@@ -1,17 +1,22 @@
 --List all data for all customers
 
-select * from customers;
+select * from Customers;
 
 
 -- List the name and city of agents named smith
 
 select name, city from Agents
-where name = 'Smith'
+where name = 'Smith';
 
 --List pid, name, and quantity of products costing more than us 1.25
 
 select pid, name,  quantity from Products
-where priceusd > 1.25
+where priceusd > 1.25;
 
 --List the ordno and aid of all orders
-select ordno, aid from orders
+select ordno, aid from Orders;
+
+--List the names and cities of customers not in Dallas.
+
+select name, city from Customers
+where city !== 'Dallas';
