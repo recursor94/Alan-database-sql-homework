e3b6686b12d84b79a4721ba869254fa7dbb65cd3
recursor94 <andrew.spano1@marist.edu>
2013-09-14 21:55:56 -0400

    completed query 3

--- assignment1.sql
+++ assignment1.sql
@@ -4,3 +4,11 @@ select * from customers;
 
 
 -- List the name and city of agents named smith
+
+select name, city from Agents
+where name = 'Smith'
+
+--List pid, name, and quantity of products costing more than us 1.25
+
+select pid, name,  quantity from Products
+where priceusd > 1.25
