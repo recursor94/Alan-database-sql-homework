32532571e1847b47c6531757e78bbf6289f79599
recursor94 <andrew.spano1@marist.edu>
2013-09-23 00:10:23 -0400

    finished problem 4

--- assignment2.sql
+++ assignment2.sql
@@ -15,3 +15,12 @@ select distinct cid, name from Customers
 where cid not in (
       select distinct cid from orders
       where aid = 'a03');
+
+select distinct cid, name from Customers
+where cid in (
+      select cid from Orders
+      where pid = 'p01' or pid = 'p02');
+
+select distinct pid from Products
+where cid in (
+      
