f1e2fb72559084b8e57671e9be07c926deddcd0f
recursor94 <andrew.spano1@marist.edu>
2013-09-22 23:20:12 -0400

    completed first three problems

--- assignment2.sql
+++ assignment2.sql
@@ -1,8 +1,7 @@
 select distinct city from Agents 
 where aid in(
        select aid  from Orders
-       where cid = 'c002'
-);
+       where cid = 'c002');
 
 select distinct pid from Orders 
 where aid in (
@@ -11,3 +10,8 @@ where aid in (
       	    (
 		select cid from Customers
 		where city = 'Kyoto'));
+
+select distinct cid, name from Customers
+where cid not in (
+      select distinct cid from orders
+      where aid = 'a03');
