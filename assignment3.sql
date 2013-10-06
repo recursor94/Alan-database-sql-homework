195283cf79b50226c739f7bc863462995878f56b
recursor94 <andrew.spano1@marist.edu>
2013-10-06 18:44:02 -0400

    added comment labeling question 2

--- assignment3.sql
+++ assignment3.sql
@@ -1,3 +1,4 @@
+--Get the cities of agents booking an order for customer c002.  THis time use joins; no subqueries.
 select city from Agents a
 inner join Orders o
       on o.aid = a.aid and o.cid='c002';
